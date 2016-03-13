91f01f0414ff125f4b25c04195a07f6a87ba224f
Joshua Tompkins <josh@joshtompkins.com>
2016-03-13 17:57:49 -0400

    Add README.

---
+++ README.md
@@ -0,0 +1,87 @@
+# war-game-js
+
+A simple program for simulating the [card game War](https://www.pagat.com/war/war.html).
+
+## Setup
+
+1. Clone the repo.
+2. Make sure you've got NodeJS 5.0 or better and NPM installed.
+2. `npm install`
+
+## Running the Game
+
+`npm start`
+
+## Running the Tests
+
+There are 33 Mocha/Should tests. Run them using NPM:
+
+`npm test`
+
+## System Design
+
+The game of War is relatively simple, and our architecture reflects that. The system is built from four simple components, arranged in a linear dependency tree:
+
+````
++--------+    +--------+    +----------+    +---------------+
+|        |    |        |    |          |    |               |
+|  Card  +---->  Deck  +---->  Player  +---->  GameManager  |
+|        |    |        |    |          |    |               |
++--------+    +--------+    +----------+    +---------------+
+````
+
+Each component is implemented as an ES6 class. In practice, they're pretty unremarkable, but were a few interesting decisions to be made:
+
+### What happens if you run out of cards during a War?
+
+The [game rules](https://www.pagat.com/war/war.html) are ambigious with regards to what happens if a player runs out of cards during a war, and note a couple of possible options:
+
+1. A player who runs out of cards is eliminated from the game
+2. The player's last card is used as the "top" card for the war.
+
+I went with option #1, mostly because it was easier to implement.
+
+### Who gets to participate in a multi-player War?
+
+According to the rules, in War games with three or more players, if a War occurs involving fewer than the total number of players in the game, *all* players participate in the war. That wasn't my recollection of how the game is played, but a) the spec says to do it that way and b) it definitely seems to make the games go faster.
+
+### Man, these games take a long time!
+
+Sometimes, unit tests just don't tell you everything. With all the code written and all tests passing, I settled in to test a full game, sticking with just two players. I told the game to start, and waited.
+
+And waited.
+
+And waited.
+
+103,267 turns later, the game finally finished. I started the game again. I wish I could tell you how many turns it took to finish, but I don't know - I killed the process after 5 minutes.
+
+The rules mention games can take a long time, but that didn't seem right. Googling "expected game length of war" told me that a surprising amount of research has been done on the game - including one critical point: there are many game states that are both stable and cyclical. These stable states seem to stem from a surprisingly boring place - the order of the cards when they're placed in the winning player's deck after a war.
+
+My initial implementation was straightforward, placing the cards on the bottom of the deck in player order. Unfortunately, discussion on [this stack exchange question](http://mathoverflow.net/questions/11503/does-war-have-infinite-expected-length/) indicated that player-ordering the cards was one of the things that frequently led to the cyclic states. [One answer](http://mathoverflow.net/a/31185) cited research suggesting that randomizing the order of the cards as they entered the winning player's deck tended toward finite-length games, so that's what I do. Most games I tried now ended in 200-ish games, which is around the expected length predicted by simulation in [this article](http://www.esorensen.com/2009/10/26/the-science-of-war/).
+
+### What's a `*[Symbol.iterator]`?
+
+The `GameManager` class maintains game state - the active players, round number, etc. `GameManager` can happily execute a game turn, but I wanted a way to easily drive the game from outside `GameManager` while still being able to report the progress of the game.
+
+What I decided to do was use a new ES6 feature and make the `GameManager` [iterable](http://www.2ality.com/2015/02/es6-iteration.html), allowing it to be run to completion using the new `for-of` loop and yielding an object to report the current game state each turn.
+
+Implementing an iterator is simple:
+
+````
+*[Symbol.iterator]() {
+  while (!this.isGameComplete)
+    yield this.playTurn();
+}
+````
+
+...and actually running the game is as easy as looping over the `GameManager` instance:
+
+````
+const manager = new GameManager(...);
+
+for (const round of manager) {
+  // report round results here!
+}
+````
+
+This is cutting-edge in the JavaScript world, but anyone used to Ruby's blocks is probably pretty unimpressed right now.
--- src/gameManager.js
+++ src/gameManager.js
@@ -4,207 +4,207 @@ import _ from 'lodash';
  * A manager of game state for the card game War.
  */
 export default class GameManager {
-    /**
-     * Initialize the game manager and prepare the game state for play.
-     * @constructor
-     * @param {Array} players - The players participating.
-     */
-    constructor(players) {
-      this._activePlayers = players;
-      this._eliminatedPlayers = [];
-      this._round = 0;
-    }
+  /**
+   * Initialize the game manager and prepare the game state for play.
+   * @constructor
+   * @param {Array} players - The players participating.
+   */
+  constructor(players) {
+    this._activePlayers = players;
+    this._eliminatedPlayers = [];
+    this._round = 0;
+  }
 
-    /**
-     * Is this game complete?
-     * NOTE: Does not distinguish between a game with a winner and
-     * a tied game.
-     * @returns {boolean}
-     */
-    get isGameComplete() {
-      return this._activePlayers.length <= 1;
-    }
+  /**
+   * Is this game complete?
+   * NOTE: Does not distinguish between a game with a winner and
+   * a tied game.
+   * @returns {boolean}
+   */
+  get isGameComplete() {
+    return this._activePlayers.length <= 1;
+  }
 
-    /**
-     * Did the game finish in a tie?
-     * @returns {boolean}
-     */
-    get isGameTied() {
-      return this.isGameComplete && this._activePlayers.length < 1;
-    }
+  /**
+   * Did the game finish in a tie?
+   * @returns {boolean}
+   */
+  get isGameTied() {
+    return this.isGameComplete && this._activePlayers.length < 1;
+  }
 
-    /**
-     * Gets the winning {@link Player}.
-     * @returns {Object} - The winning player if there is one; null otherwise.
-     */
-    get winner() {
-      if (this.isGameComplete && !this.isGameTied)
-        return this._activePlayers[0];
+  /**
+   * Gets the winning {@link Player}.
+   * @returns {Object} - The winning player if there is one; null otherwise.
+   */
+  get winner() {
+    if (this.isGameComplete && !this.isGameTied)
+      return this._activePlayers[0];
 
-      return null;
-    }
+    return null;
+  }
 
-    /**
-     * Gets the current round.
-     * @returns {number} - The current round.
-     */
-    get round() {
-      return this._round;
-    }
+  /**
+   * Gets the current round.
+   * @returns {number} - The current round.
+   */
+  get round() {
+    return this._round;
+  }
 
-    /**
-     * Given a set of cards, find the winners.
-     * @param {Array} - An arry of object tuples in the form of {player, card}.
-     * @returns {Array} - An array containing the winning tuple. In the case of a tie,
-     * multiple tuples are returned.
-     */
-    findWinner(cards) {
-      let sortedCards = cards.sort((l, r) => {
-        // the following looks weird, I know - but we want the *highest*
-        // valued cards to appear at the beginning of the sorted list,
-        // which means that we must sort a *higher value* to be *lower*.
-        if (l.card.value > r.card.value) return -1;
-        if (l.card.value < r.card.value) return 1;
+  /**
+   * Given a set of cards, find the winners.
+   * @param {Array} - An arry of object tuples in the form of {player, card}.
+   * @returns {Array} - An array containing the winning tuple. In the case of a tie,
+   * multiple tuples are returned.
+   */
+  findWinner(cards) {
+    let sortedCards = cards.sort((l, r) => {
+      // the following looks weird, I know - but we want the *highest*
+      // valued cards to appear at the beginning of the sorted list,
+      // which means that we must sort a *higher value* to be *lower*.
+      if (l.card.value > r.card.value) return -1;
+      if (l.card.value < r.card.value) return 1;
 
-        return 0;
-      });
+      return 0;
+    });
 
-      return sortedCards.filter(t => t.card.value === sortedCards[0].card.value);
-    }
+    return sortedCards.filter(t => t.card.value === sortedCards[0].card.value);
+  }
 
-    /**
-     * Gathers cards from a list of players as preparation for a
-     * game round. Finds players that should be eliminated, and
-     * puts cards to be compared in the format required by {@link GameManager#findWinner}.
-     * NOTE: this method follows "possibility 1" for the situation from
-     * https://www.pagat.com/war/war.html for the situation where a player
-     * runs out of cards while drawing cards for a war.
-     * @private
-     * @param {Array} players - A list of players that will participate
-     * @param {number} numberOfCards - The number of cards required to particpate
-     * @returns {Object} - An object tuple in the form of {comparedCards, playedCards, eliminatedPlayers}
-     */
-    gatherCardsForComparison(players, numberOfCards) {
-      let playedCards = [];
-      let comparedCards = [];
-      let eliminatedPlayers = [];
+  /**
+   * Gathers cards from a list of players as preparation for a
+   * game round. Finds players that should be eliminated, and
+   * puts cards to be compared in the format required by {@link GameManager#findWinner}.
+   * NOTE: this method follows "possibility 1" for the situation from
+   * https://www.pagat.com/war/war.html for the situation where a player
+   * runs out of cards while drawing cards for a war.
+   * @private
+   * @param {Array} players - A list of players that will participate
+   * @param {number} numberOfCards - The number of cards required to particpate
+   * @returns {Object} - An object tuple in the form of {comparedCards, playedCards, eliminatedPlayers}
+   */
+  gatherCardsForComparison(players, numberOfCards) {
+    let playedCards = [];
+    let comparedCards = [];
+    let eliminatedPlayers = [];
 
-      players.forEach(p => {
-        if (!p.canTake(numberOfCards)) {
-          // if the player can't give all the required cards,
-          // they're eliminated from the game. take whatever
-          // cards they have left and add them to the list of
-          // cards played in the round.
-          playedCards.push(...p.take(numberOfCards));
-          eliminatedPlayers.push(p);
-        }
-        else {
-          // the player has at least the required number of
-          // cards left. take the cards and drop them into
-          // the list of cards played this round. the last
-          // drawn card is entered into the comparison against
-          // the other players.
-          let cards = p.take(numberOfCards);
+    players.forEach(p => {
+      if (!p.canTake(numberOfCards)) {
+        // if the player can't give all the required cards,
+        // they're eliminated from the game. take whatever
+        // cards they have left and add them to the list of
+        // cards played in the round.
+        playedCards.push(...p.take(numberOfCards));
+        eliminatedPlayers.push(p);
+      }
+      else {
+        // the player has at least the required number of
+        // cards left. take the cards and drop them into
+        // the list of cards played this round. the last
+        // drawn card is entered into the comparison against
+        // the other players.
+        let cards = p.take(numberOfCards);
 
-          playedCards.push(...cards);
-          comparedCards.push({ player: p, card: cards[cards.length - 1] });
-        }
-      });
+        playedCards.push(...cards);
+        comparedCards.push({ player: p, card: cards[cards.length - 1] });
+      }
+    });
 
-      return {comparedCards, playedCards, eliminatedPlayers};
-    }
+    return {comparedCards, playedCards, eliminatedPlayers};
+  }
 
-    /**
-    * Resolve a round of the game. Each player will draw cards and a winner will be
-    * determined. In the case of a tie, a series of Wars will be resolved until
-    * a winner is determined. In either case, once a winner is found, the winning
-    * player receives all cards played in the round.
-    * @param {Array} players - The participating players
-    * @param {number} numberOfCards - The number of cards that will be played initially.
-    * @param {boolean} awardCards - Whether or not cards should be awarded.
-    * @returns {Object} - An object tuple in the form of {winner, playedCards, eliminatedPlayers}.
-    */
-    resolveRound(players, numberOfCards = 1, awardCards = true) {
-      let winner = null;
+  /**
+  * Resolve a round of the game. Each player will draw cards and a winner will be
+  * determined. In the case of a tie, a series of Wars will be resolved until
+  * a winner is determined. In either case, once a winner is found, the winning
+  * player receives all cards played in the round.
+  * @param {Array} players - The participating players
+  * @param {number} numberOfCards - The number of cards that will be played initially.
+  * @param {boolean} awardCards - Whether or not cards should be awarded.
+  * @returns {Object} - An object tuple in the form of {winner, playedCards, eliminatedPlayers}.
+  */
+  resolveRound(players, numberOfCards = 1, awardCards = true) {
+    let winner = null;
 
-      let {
-        comparedCards,
-        playedCards,
-        eliminatedPlayers } = this.gatherCardsForComparison(players, numberOfCards);
+    let {
+      comparedCards,
+      playedCards,
+      eliminatedPlayers } = this.gatherCardsForComparison(players, numberOfCards);
 
-      // everyone can be eliminated at once, so make sure we actually had
-      // some cards to compare.
-      if (comparedCards.length > 0) {
-        let result = this.findWinner(comparedCards);
+    // everyone can be eliminated at once, so make sure we actually had
+    // some cards to compare.
+    if (comparedCards.length > 0) {
+      let result = this.findWinner(comparedCards);
 
-        if (result.length > 1) {
-          // LET SLIP THE DOGS OF WAR
-          // NOTE: we don't award cards until all wars have been resolved
-          // additionally, according to https://www.pagat.com/war/war.html, ALL
-          // players participate in the war if there's a tie, not just the people
-          // who tied.
-          let tieResult = this.resolveRound(players, 3, false);
+      if (result.length > 1) {
+        // LET SLIP THE DOGS OF WAR
+        // NOTE: we don't award cards until all wars have been resolved
+        // additionally, according to https://www.pagat.com/war/war.html, ALL
+        // players participate in the war if there's a tie, not just the people
+        // who tied.
+        let tieResult = this.resolveRound(players, 3, false);
 
-          winner = tieResult.winner;
-          playedCards.push(...tieResult.playedCards);
-          eliminatedPlayers.push(...tieResult.eliminatedPlayers);
+        winner = tieResult.winner;
+        playedCards.push(...tieResult.playedCards);
+        eliminatedPlayers.push(...tieResult.eliminatedPlayers);
 
-          // NOTE: the game manager object probably shouldn't be responsible for
-          // displaying the results of individual comparisons, but if you really
-          // want to see how wars are resolved, uncomment the following line.
-          // console.log(`WAR! ${winner.name} won ${tieResult.playedCards.length} cards.`);
-        }
-        else {
-          winner = result[0].player;
-        }
-
-        // remember, the war can also result in everyone being eliminated,
-        // so don't award cards if there wasn't a winner. we also only
-        // want to award cards if told to do so. in general, we only want
-        // to award cards once the round is over and all of the possible
-        // wars have been resolved.
-
-        // NOTE: a single game of war can continue for a very long time, especially
-        // if a particular player's cards are added to the bottom of the winner's
-        // stack first consistently. according to http://mathoverflow.net/a/31185,
-        // randomizing the order in which cards are added to the bottom of the stack
-        // tends towards a finite game length, so follow that here.
-        if (winner && awardCards)
-          winner.give(_.shuffle(playedCards));
+        // NOTE: the game manager object probably shouldn't be responsible for
+        // displaying the results of individual comparisons, but if you really
+        // want to see how wars are resolved, uncomment the following line.
+        // console.log(`WAR! ${winner.name} won ${tieResult.playedCards.length} cards.`);
+      }
+      else {
+        winner = result[0].player;
       }
 
-      return {winner, playedCards, eliminatedPlayers};
+      // remember, the war can also result in everyone being eliminated,
+      // so don't award cards if there wasn't a winner. we also only
+      // want to award cards if told to do so. in general, we only want
+      // to award cards once the round is over and all of the possible
+      // wars have been resolved.
+
+      // NOTE: a single game of war can continue for a very long time, especially
+      // if a particular player's cards are added to the bottom of the winner's
+      // stack first consistently. according to http://mathoverflow.net/a/31185,
+      // randomizing the order in which cards are added to the bottom of the stack
+      // tends towards a finite game length, so follow that here.
+      if (winner && awardCards)
+        winner.give(_.shuffle(playedCards));
     }
 
-    /**
-     * Play a round of war. This method keeps track of in-game state, and
-     * moves players from the active to the eliminated list as they run out
-     * of cards.
-     * @returns {Object} - An object tuple of the form {round, winner, eliminatedPlayers}
-     */
-    playTurn() {
-      let {winner, eliminatedPlayers} = this.resolveRound(this._activePlayers);
+    return {winner, playedCards, eliminatedPlayers};
+  }
 
-      eliminatedPlayers.forEach(p => {
-        let index = this._activePlayers.findIndex(ap => ap.name === p.name);
+  /**
+   * Play a round of war. This method keeps track of in-game state, and
+   * moves players from the active to the eliminated list as they run out
+   * of cards.
+   * @returns {Object} - An object tuple of the form {round, winner, eliminatedPlayers}
+   */
+  playTurn() {
+    let {winner, eliminatedPlayers} = this.resolveRound(this._activePlayers);
 
-        if (index > -1) {
-          this._activePlayers.splice(index, 1);
-          this._eliminatedPlayers.push(p);
-        }
-      });
+    eliminatedPlayers.forEach(p => {
+      let index = this._activePlayers.findIndex(ap => ap.name === p.name);
 
-      this._round = this._round + 1;
+      if (index > -1) {
+        this._activePlayers.splice(index, 1);
+        this._eliminatedPlayers.push(p);
+      }
+    });
 
-      return {round: this._round, winner, eliminatedPlayers};
-    }
+    this._round = this._round + 1;
 
-    *[Symbol.iterator]() {
-      while (!this.isGameComplete)
-        yield this.playTurn();
-    }
+    return {round: this._round, winner, eliminatedPlayers};
+  }
 
-    toString() {
-      return this._activePlayers.map(p => p.toString()).join('\n');
-    }
+  *[Symbol.iterator]() {
+    while (!this.isGameComplete)
+      yield this.playTurn();
+  }
+
+  toString() {
+    return this._activePlayers.map(p => p.toString()).join('\n');
+  }
 }
